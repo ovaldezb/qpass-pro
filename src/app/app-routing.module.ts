28f8ed9085d8933a15b2e55d221eb446465ef7a2
ovaldezb <omar.valdez.becerril@gmail.com>
2022-10-01 09:44:03 -0500

    dividir modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgendaComponent } from './components/agenda/agenda.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { InvitacionesComponent } from './components/invitaciones/invitaciones.component';
 
 const routes: Routes = [
-  {path:'directorio', component: AgendaComponent}
+  {path:'', component: LayoutComponent},
+  {path:'directorio', component: AgendaComponent},
+  {path:'invitaciones', component: InvitacionesComponent}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+@NgModule(
+  {imports: [RouterModule.forRoot(routes)], exports: [RouterModule]}
+  )
 export class AppRoutingModule { }
