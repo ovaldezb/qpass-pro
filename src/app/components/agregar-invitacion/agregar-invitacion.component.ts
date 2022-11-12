d07889f805602f065eff395865fbf2f056f40df4
ovaldezb <omar.valdez.becerril@gmail.com>
2022-11-12 12:04:26 -0600

    CRUD working

--- src/app/components/agregar-invitacion/agregar-invitacion.component.ts
+++ src/app/components/agregar-invitacion/agregar-invitacion.component.ts
@@ -1,20 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 import { Invitacion } from 'src/app/models/invitacion';
+import { InvitacionesService } from 'src/app/services/invitaciones/invitaciones.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-invitacion',
   templateUrl: './agregar-invitacion.component.html',
-  styleUrls: ['./agregar-invitacion.component.scss']
+  styleUrls: ['./agregar-invitacion.component.scss'],
+  providers: [InvitacionesService]
 })
 export class AgregarInvitacionComponent implements OnInit {
 
-  public invitacion:Invitacion=new Invitacion("", "Omar","Invitacion",10,"","","", new Date(),0, "","area común",true);
-  constructor() { }
+  @Input() invitacion : Invitacion=new Invitacion("", "","",0,"","","", new Date(),0, "","",false);
+  @Output() cancelStatus = new EventEmitter();
+  public action:String="Guardar";
+  constructor(private invitacionService: InvitacionesService) { }
 
   ngOnInit(): void {
+    console.log(this.invitacion._id);
+    if(this.invitacion._id !== ''){
+      this.action = 'Actualizar';
+    }
   }
 
   agregarInvitacion():void{
-    console.log(this.invitacion);
+    if(this.action==='Guardar'){
+      Swal.fire({
+        title:'Desea agregar ésta Invitación?',
+        showCancelButton:true,
+        confirmButtonText:'Agregar'
+      }).then((result)=>{
+        if(result.isConfirmed){
+          this.invitacionService.addInvitation(this.invitacion).subscribe((res)=>{
+            this.cancelStatus.emit({load:true});
+          });
+        }
+      });
+    }else{
+      Swal.fire({
+        title:'Desea actualizar ésta Invitacion?',
+        showCancelButton:true,
+        confirmButtonText:'Actualizar'
+      }).then((result)=>{
+        if(result.isConfirmed){
+          this.invitacionService.updateInvitacion(this.invitacion._id,this.invitacion).subscribe((res)=>{
+            this.cancelStatus.emit({load:true});
+          });
+        }
+      });
+    }
+  }
+
+  cancelAction():void{
+    this.invitacion = new Invitacion("", "","",0,"","","", new Date(),0, "","",false);
+    this.cancelStatus.emit({load:false});
   }
 }
--- src/app/components/lista-invitaciones/lista-invitaciones.component.ts
+++ src/app/components/lista-invitaciones/lista-invitaciones.component.ts
@@ -3,6 +3,7 @@ import { Invitacion } from 'src/app/models/invitacion';
 import { InvitacionesService } from 'src/app/services/invitaciones/invitaciones.service';
 import { Router } from '@angular/router';
 import { error } from '@angular/compiler/src/util';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lista-invitaciones',
@@ -12,29 +13,78 @@ import { error } from '@angular/compiler/src/util';
 })
 export class ListaInvitacionesComponent implements OnInit {
 
+  public HighlightRow: number = -1;
+  public isSelected: boolean =false;
   public isAdd:boolean=false;
   public isList:boolean=true;
   public invitaciones:Invitacion[]=[];
-  public invitacion:Invitacion=new Invitacion("","Omar","Invitacion",10,"","","", new Date(),0, "","",false);
+  public invitacion:Invitacion=new Invitacion("","","",0,"","","", new Date(),0, "","",false);
   
   constructor(private _router : Router, private invitacionService: InvitacionesService) { 
 
   }
 
   ngOnInit(): void {
+    this.isSelected=false;
     this.invitacionService.getInvitaciones().subscribe(
         (res)=>{
-          console.log(res);
+          //console.log(res);
+          if(res.length > 0){
+            this.invitaciones = res;
+            this.isList = true;
+            this.isAdd = false;
+          }else{
+            this.isList = false;
+            this.isAdd = true;
+          }
         }
       );
-    if(this.invitaciones.length === 0){
-      this.isList = false;
-      this.isAdd = true;
+  }
+
+  selectRow(index: number): void {
+    this.HighlightRow = index;
+    this.isSelected = true;
+  }
+  deleteInvitacion():void{
+    swal.fire({
+      title:'Desea eliminar esta Invitacion',
+      showCancelButton:true,
+      confirmButtonText:'Borrar'
+    }).then((result)=>{
+      if(result.isConfirmed){
+        this.invitacionService.deleteInvitacion(this.invitaciones[this.HighlightRow]._id).subscribe(res=>{
+          this.loadInvitaciones();
+          swal.fire('La invitación se ha eliminado correctamente');
+        });
+      }
+    });
+
+  }
+  agregarInvitacion():void{
+    this.isList = false;
+    this.isAdd = true;
+  }
+
+  updateInvitacion():void{
+    this.invitacion = this.invitaciones[this.HighlightRow];
+    this.isList = false;
+    this.isAdd = true;
+  }
+
+  cancelStatus(event:any):void{
+    this.isList = true;
+    this.isAdd = false;
+    this.HighlightRow = -1;
+    console.log(event);
+    if(event.load){
+      this.loadInvitaciones();
     }
   }
 
-  agregarInvitacion():void{
-
+  loadInvitaciones():void{
+    this.invitacionService.getInvitaciones().subscribe((res)=>{
+      this.invitaciones = res;
+    });
   }
 
 }
