75910f3362f8686ec02dfcfd766808dfd669f917
ovaldezb <omar.valdez.becerril@gmail.com>
2023-03-27 16:44:23 -0600

    add more items to Invitacion

--- src/app/models/invitacion.ts
+++ src/app/models/invitacion.ts
@@ -11,6 +11,10 @@ export class Invitacion{
     public horaEvento: number,
     public detalle:string,
     public lugar:string,
-    public notificarEntrada:boolean
+    public notificarEntrada:boolean,
+    public placas: string,
+    public tarjeton: string,
+    public horaIngreso: Date,
+    public dejaId: boolean
   ){}
 }
