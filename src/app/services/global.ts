5e4d85aceae4d1aa6a7a6f98fb2d56762bea0f52
ovaldezb <omar.valdez.becerril@gmail.com>
2023-03-29 16:11:04 -0600

    update url

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var Global ={
-  url: 'https://wvtg7wi5fj.execute-api.us-east-1.amazonaws.com/dev/invitacion',
+  url: 'https://l744i40nsf.execute-api.us-east-1.amazonaws.com/dev/invitacion',
   GUARDAR:'Guardar',
   ACTUALIZAR: 'Actualizar',
   AGREGAR: 'Agregar'
