87972b5c8cfdfdb95e4003c1e652877d0dba497d
hheisego <hheisego@gmail.com>
2023-03-31 17:47:37 -0600

    condominios 70%

--- src/app/components/add-edit-condominios/add-edit-condominios.component.ts
+++ src/app/components/add-edit-condominios/add-edit-condominios.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CondominiosService } from 'src/app/services/condominios/condominios.service';
 
+
 @Component({
   selector: 'app-add-edit-condominios',
   templateUrl: './add-edit-condominios.component.html',
@@ -10,24 +11,52 @@ import { CondominiosService } from 'src/app/services/condominios/condominios.ser
 export class AddEditCondominiosComponent implements OnInit {
 
   @Input() mission = ''; 
-  
+  @Input() condo: any;
+
   condosGroupForm!: FormGroup;
   apiResponse: any;
   apiRequest: any;
   taskMission: boolean = true;
-
+  listaCondos: any;
+  
+  
+  
   constructor(private condosInfo: FormBuilder,
-              private service: CondominiosService
+              private service: CondominiosService,
+           
               ) { }
 
   ngOnInit(): void {
 
+    console.log(this.condo);
+    console.log(this.mission);
+    
+    if (this.mission == "Editar Condominio"){
+
+      console.log("entra aqui")
+
+      this.condosGroupForm = this.condosInfo.group({
+
+        condoId: [this.condo["id"], Validators.required],
+        condominio: [this.condo["Condominio"], Validators.required],
+        direccion: [this.condo["Direccion"], Validators.required]
+
+      });
+
+
+    }
+    else if (this.mission == "Agregar Condominio"){
+
     this.condosGroupForm = this.condosInfo.group({
 
+      condoId: ['', Validators.required],
       condominio: ['', Validators.required],
       direccion: ['', Validators.required]
 
     });
+    
+    }   
+    
 
   }
 
@@ -51,7 +80,7 @@ export class AddEditCondominiosComponent implements OnInit {
 
 
     this.condosGroupForm.reset();
-
+  
 
   }
 
@@ -60,7 +89,18 @@ export class AddEditCondominiosComponent implements OnInit {
 
     this.condosGroupForm.reset();
     this.taskMission = false;
+    
 
   }
+
+  refreshCondos() {
+
+    this.service.getCondos().subscribe(data => {
+
+      this.listaCondos = data;
+
+    });
+  }
+
 
 }
--- src/app/components/lista-condominios/lista-condominios.component.ts
+++ src/app/components/lista-condominios/lista-condominios.component.ts
@@ -1,39 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CondominiosService } from 'src/app/services/condominios/condominios.service';
 
 @Component({
   selector: 'app-lista-condominios',
   templateUrl: './lista-condominios.component.html',
   styleUrls: ['./lista-condominios.component.scss']
 })
+
+
 export class ListaCondominiosComponent implements OnInit {
 
   ModalTitle: string = '';
-  taskMission: boolean = true;
-
-  condominio: string = '';
+  taskMission: boolean = false;
+  listaCondos: any;
+  condominio: any;
   direccion: string = '';
+  
 
-  constructor() { }
+
+  constructor(private service: CondominiosService) { }
+
 
   ngOnInit(): void {
 
+    this.refreshCondos();
 
   }
 
-
-  closeModal() {
-
-    this.taskMission = false;
-    //this.refreshUsers();
-
-  }
 
   agregarCondo(){
 
+    
     this.ModalTitle = "Agregar Condominio";
-
     this.taskMission = true;
+    //this.refreshCondos();
+
   }
+
+  editarCondo(condo: any){
+
+    this.condominio = condo;
+    this.taskMission = true;
+    this.ModalTitle = "Editar Condominio";
+    
+
+  }
+
+  closeModal() {
+
+
+    this.refreshCondos();
+    this.taskMission = false;
+
+
+  }
+
+  refreshCondos() {
+
+    this.service.getCondos().subscribe(data => {
+
+      this.listaCondos = data;
+
+    });
+  }
+
 
 }
