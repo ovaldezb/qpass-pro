a6c9870e5c5caf5001caa0d1035c1b9a8ec75bd3
hheisego <hheisego@gmail.com>
2023-05-02 18:44:51 -0600

    ultimos cambios

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,7 @@
 export var Global ={
   url: 'https://9983wsq7dl.execute-api.us-east-1.amazonaws.com/dev/invitacion',
+  urlUsuarios: 'https://9983wsq7dl.execute-api.us-east-1.amazonaws.com/dev/usuario',
+  urlCondominios: 'https://9983wsq7dl.execute-api.us-east-1.amazonaws.com/dev/condominio',
   GUARDAR:'Guardar',
   ACTUALIZAR: 'Actualizar',
   AGREGAR: 'Agregar'
