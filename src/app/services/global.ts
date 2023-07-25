ce27af9713f31c2765593b4b1ec80d1268869c0c
Sari Fernandez <sarifern@cisco.com>
2023-07-25 17:59:53 -0600

    updating URL values

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,9 +1,11 @@
-export var Global ={
-
-  urlInvitaciones: 'https://i1nu7khbcg.execute-api.us-east-1.amazonaws.com/dev/invitacion',
-  urlUsuarios: 'https://lw5e5ki809.execute-api.us-east-1.amazonaws.com/dev/usuario',
-  urlCondominios: 'https://ipz5z3hru6.execute-api.us-east-1.amazonaws.com/dev/condominio',
-  GUARDAR:'Guardar',
+export var Global = {
+  urlInvitaciones:
+    'https://1em6anv6rc.execute-api.us-east-1.amazonaws.com/dev/invitacion',
+  urlUsuarios:
+    'https://swtyv3hobb.execute-api.us-east-1.amazonaws.com/dev/usuario',
+  urlCondominios:
+    'https://mpmqtc5gql.execute-api.us-east-1.amazonaws.com/dev/condominio',
+  GUARDAR: 'Guardar',
   ACTUALIZAR: 'Actualizar',
-  AGREGAR: 'Agregar' ,
+  AGREGAR: 'Agregar',
 };
