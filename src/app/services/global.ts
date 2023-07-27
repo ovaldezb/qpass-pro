5dd839f410d79d0642e6ce3760bbf90f831f7941
Sari Fernandez <sarifern@cisco.com>
2023-07-27 17:06:42 -0600

    updating global URL from API Gateways

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,10 +1,8 @@
 export var Global = {
   urlInvitaciones:
-    'https://1em6anv6rc.execute-api.us-east-1.amazonaws.com/dev/invitacion',
-  urlUsuarios:
-    'https://swtyv3hobb.execute-api.us-east-1.amazonaws.com/dev/usuario',
-  urlCondominios:
-    'https://mpmqtc5gql.execute-api.us-east-1.amazonaws.com/dev/condominio',
+    'https://1em6anv6rc.execute-api.us-east-1.amazonaws.com/dev/',
+  urlUsuarios: 'https://swtyv3hobb.execute-api.us-east-1.amazonaws.com/dev/',
+  urlCondominios: 'https://mpmqtc5gql.execute-api.us-east-1.amazonaws.com/dev/',
   GUARDAR: 'Guardar',
   ACTUALIZAR: 'Actualizar',
   AGREGAR: 'Agregar',
